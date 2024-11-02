18c09a0fd027c0630f509b437696064767fdd5fd
Pereirapam <pereirapamela690@gmail.com>
2024-11-02 17:47:07 -0300

    todas as rotas criadas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MaterialController;
+use App\Http\Controllers\BugdetController;
+use App\Http\Controllers\ProfessionalController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +21,56 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    //Routes of tasks
+    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+    Route::post('/tasks', [TaskController::class, 'store'])->name('task.store');
+    Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
+    Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+
+
+    //Routes of categories
+    Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+    Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+    Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+    Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
+    Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+    Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+
+
+    //Routes of material
+    Route::get('/materials', [MaterialController::class, 'index'])->name('materials.index');
+    Route::get('/materials/create', [MaterialController::class, 'create'])->name('materials.create');
+    Route::post('/materials', [MaterialController::class, 'store'])->name('materials.store');
+    Route::get('/materials/{material}', [MaterialController::class, 'show'])->name('materials.show');
+    Route::get('/materials/{material}/edit', [MaterialController::class, 'edit'])->name('materials.edit');
+    Route::put('/materials/{material}', [MaterialController::class, 'update'])->name('materials.update');
+    Route::delete('/materials/{material}', [MaterialController::class, 'destroy'])->name('materials.destroy');
+
+
+    //Routes of bugdet
+    Route::get('/bugdets', [BugdetController::class, 'index'])->name('bugdets.index');
+    Route::get('/bugdets/create', [BugdetController::class, 'create'])->name('bugdets.create');
+    Route::post('/bugdets', [BugdetController::class, 'store'])->name('bugdets.store');
+    Route::get('/bugdets/{bugdet}', [BugdetController::class, 'show'])->name('bugdets.show');
+    Route::get('/bugdets/{bugdet}/edit', [BugdetController::class, 'edit'])->name('bugdets.edit');
+    Route::put('/bugdets/{bugdet}', [BugdetController::class, 'update'])->name('bugdets.update');
+    Route::delete('/bugdets/{bugdet}', [BugdetController::class, 'destroy'])->name('bugdets.destroy');
+
+
+    //Routes of professionals
+    Route::get('/professionals', [ProfessionalController::class, 'index'])->name('professionals.index');
+    Route::get('/professionals/create', [ProfessionalController::class, 'create'])->name('professionals.create');
+    Route::post('/professionals', [ProfessionalController::class, 'store'])->name('professionals.store');
+    Route::get('/professionals/{professional}', [ProfessionalController::class, 'show'])->name('professionals.show');
+    Route::get('/professionals/{professional}/edit', [ProfessionalController::class, 'edit'])->name('professionals.edit');
+    Route::put('/professionals/{professional}', [ProfessionalController::class, 'update'])->name('professionals.update');
+    Route::delete('/professionals/{professional}', [ProfessionalController::class, 'destroy'])->name('professionals.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
